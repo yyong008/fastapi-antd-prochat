964f3af7ceb310672cf15d75e7258eed7c9dc5c3
Magnesium- <yyong007@outlook.com>
2024-11-12 12:24:39 +0800

    ✨ chroma

--- backend/backend/langchain/rag/vector/vector_chroma.py
+++ backend/backend/langchain/rag/vector/vector_chroma.py
@@ -2,13 +2,22 @@ from langchain_chroma import Chroma
 
 
 class VectorChroma:
-  def create_chroma_storage(self,texts, embeddings):
-    vector_store = Chroma.from_texts(texts, embeddings)
-    return vector_store
-  
-  def get_chroma_storage(self, collection_name,embeddings, persist_directory):
-    return Chroma(
-      collection_name=collection_name,
-      persist_directory=persist_directory,
-      embedding_function=embeddings
-    )
+    @classmethod
+    def create_chroma_storage_from_texts(
+        self, texts, embeddings, collection_name, persist_directory
+    ):
+        vector_store = Chroma.from_texts(
+            texts,
+            embeddings,
+            collection_name=collection_name,
+            persist_directory=persist_directory,
+        )
+        return vector_store
+
+    @classmethod
+    def get_chroma_storage(self, collection_name, embeddings, persist_directory):
+        return Chroma(
+            collection_name=collection_name,
+            persist_directory=persist_directory,
+            embedding_function=embeddings,
+        )
